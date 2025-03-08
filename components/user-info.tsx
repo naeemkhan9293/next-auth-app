c44e3bfe96eff17d9e90dd50e2cec0f8d4a5b301
saud khan <saudkhanbpk@gmail.com>
2025-03-08 12:29:17 +0500

    refactor: remove unused user prop from UserInfo component

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -1,20 +1,16 @@
-import { ExtendedUser } from "@/next-auth";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 interface UserInfoProps {
-  user?: ExtendedUser;
   label: string;
 }
 
-export const UserInfo = ({ user, label }: UserInfoProps) => {
+export const UserInfo = ({ label }: UserInfoProps) => {
   return (
     <Card className="w-[600px] shadow-md">
       <CardHeader>
         <p className="text-2xl font-semibold text-center">{label}</p>
       </CardHeader>
-      <CardContent>
-        
-      </CardContent>
+      <CardContent></CardContent>
     </Card>
   );
 };
